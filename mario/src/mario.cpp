19b426dc9b47ab7da09ca7c7d63b131083e22beb
JJGOD <jayantgodse13@gmail.com>
2025-03-21 10:08:14 +1100

    Made the pyramid printer

---
+++ mario/src/mario.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    std::cout << "input a number ";
+    std::cin >> x;
+  
+
+    for (int i = 0; i < x; i++)
+    {
+
+        std::string tri = "#";
+        
+        std::cout << tri << std::endl;
+    };
+}
